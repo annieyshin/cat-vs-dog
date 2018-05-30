ff0b75a0f8c09683e0f22a5f17e2871cd0184c09
Annie Shin and Nick Brown <nick@>
2018-05-30 15:42:34 -0700

    add background color green and default options, add pictures of kitty and puppy that show and hide based on button clicks, and add CSS styling

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,33 @@
 $(document).ready(function() {
-  $("button#cat").click(function() {
-    $("ul#cat-says").prepend("<li>Meow ~ ^.^ ~</li>");
-  });
+  $("button#green").click(function() {
+    $("body").removeClass();
+       $("body").addClass("green-background");
+    });
+
+    $("button#default").click(function() {
+  $("body").removeClass();
+  $("body").addClass("default-background");
 });
 
-  $(document).ready(function() {
+  $("button#cat").click(function() {
+    $("ul#cat-says").append("<li>Meow ~ ^.^ ~</li>");
+
+    });
+
   $("button#dog").click(function() {
     $("ul#dog-says").prepend("<li>Woof 0 - 0</li>");
+
+    });
+
+  $("button#dogpicbutton").click(function() {
+    $(".dogpic").toggle();
+
   });
+
+  $("button#catpicbutton").click(function() {
+    $(".catpic").toggle();
+
+  });
+
+
 });
