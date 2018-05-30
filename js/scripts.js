1cf3a7bcc6953acd60ea8a6c128879c38a0e54f7
Annie Shin and Nick Brown <annie@>
2018-05-30 14:46:48 -0700

    add cat and dog buttons along with lists that prepend their animal noises phonetically

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+  $("button#cat").click(function() {
+    $("ul#cat-says").prepend("<li>Meow ~ ^.^ ~</li>");
+  });
+});
+
+  $(document).ready(function() {
+  $("button#dog").click(function() {
+    $("ul#dog-says").prepend("<li>Woof 0 - 0</li>");
+  });
+});
