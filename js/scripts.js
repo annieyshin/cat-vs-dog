e8db5ee027ee0fa3f6be6402431a4a60e7d73fb3
Annie Shin and Nick Brown <nick@>
2018-05-30 15:48:34 -0700

    add .before and .after text elements to lists for dog and cat says

--- js/scripts.js
+++ js/scripts.js
@@ -11,11 +11,14 @@ $(document).ready(function() {
 
   $("button#cat").click(function() {
     $("ul#cat-says").append("<li>Meow ~ ^.^ ~</li>");
+      $("ul#cat-says").before("<br>puuuuuurrrrr<br>");
+
 
     });
 
   $("button#dog").click(function() {
     $("ul#dog-says").prepend("<li>Woof 0 - 0</li>");
+      $("ul#dog-says").after("<br>me want scooby snacks <br>");
 
     });
 
